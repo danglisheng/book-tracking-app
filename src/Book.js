7f6edb54b83e299b222dac38206a1905d953ad8a
danglisheng <3182466719@qq.com>
2018-06-14 20:30:10 +0800

    先更新服务端数据，在渲染前端页面

--- src/Book.js
+++ src/Book.js
@@ -1,38 +1,33 @@
 import React,{ Component } from 'react'
 class Book extends Component {
-  componentDidMount() {
-    /* 获取代表该Book组件的DOM节点，并将它保存为实例属性。*/
-    const bookNode=document.getElementById(`${this.props.book.id}`);
-    var curShelf=this.props.book.shelf;
-    this.bookNode=bookNode;
+  constructor(props) {
+    super(props);
+    this.dropDown=React.createRef();
+  }
+  /* 初始化下拉按钮选项 */
+  initializeDropdownItem(dropDown,curShelf){
     /* 将该Book组件里下拉列表中value值为"move"的项设为选中状态。*/
-    bookNode.querySelector(`option[value="move"]`).setAttribute("selected","");
+    dropDown.querySelector(`option[value="move"]`).setAttribute("selected","");
     /* 如果这本书在某个书架上，则将下拉列表中该书架项隐藏；*/
     if(curShelf){
-      bookNode.querySelector(`option[value=${curShelf}]`).style.display="none";
+      dropDown.querySelector(`option[value=${curShelf}]`).style.display="none";
     }
     /* 否则，把下拉列表中value值为“none”的项隐藏。*/
     else {
-      bookNode.querySelector(`option[value='none']`).style.display="none";
+      dropDown.querySelector(`option[value='none']`).style.display="none";
     }
+  }
+  componentDidMount() {
+    const dropDown=this.dropDown.current;
+    var curShelf=this.props.book.shelf;
+    this.initializeDropdownItem(dropDown,curShelf);
   }
   /* 下拉列表change事件的处理函数 */
   handleSelect(e) {
-    const prevShelf=this.props.book.shelf;
     const curShelf=e.target[e.target.selectedIndex].value;
-    const bookNode=this.bookNode;
-    /* 如果该图书以前就放在书架上，则显示之前隐藏的上个书架 */
-    if(prevShelf){
-      bookNode.querySelector(`option[value=${prevShelf}]`).style.display="block";
-    }
-    /* 若该图书以前不在书架上，则显示value值为"none"的项 */
-    else{
-      bookNode.querySelector(`option[value="none"]`).style.display="block";
-    }
-    /* 把新选中的书架隐藏 */
-    bookNode.querySelector(`option[value=${curShelf}]`).style.display="none";
+    const dropDown=this.dropDown.current;
     /* 调用定义在app.js中的回调函数 */
-    this.props.changeBookType(this.props.book,curShelf);
+    this.props.changeBookType(this.props.book,curShelf,dropDown);
     
   }
 	render() {
@@ -54,7 +49,8 @@ class Book extends Component {
                (<span></span>)}
           </div>
           <div className="book-shelf-changer">
-            <select onChange={this.handleSelect.bind(this)}>
+            <select onChange={this.handleSelect.bind(this)}
+              ref={this.dropDown}>
               <option value="move" disabled>Move to...</option>
               <option value="currentlyReading">Currently Reading</option>
               <option value="wantToRead">Want to Read</option>
